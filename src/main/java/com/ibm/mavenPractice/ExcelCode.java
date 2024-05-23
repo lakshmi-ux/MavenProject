055f422343fcf66ce4cce8c245896955df62c7d5
Emvigo <Emvigo@EM-025-MLAKSHMI>
2024-05-23 08:12:33 +0530

    change code

--- src/main/java/com/ibm/mavenPractice/ExcelCode.java
+++ src/main/java/com/ibm/mavenPractice/ExcelCode.java
@@ -11,7 +11,7 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 public class ExcelCode {
 	public static FileInputStream fs;
 	public static XSSFWorkbook wb;
-	public static XSSFSheet sh;
+	public static XSSFSheet s;
 	
 //string datas excel sheetil ullathe read cheyan ayite oru method create cheyanam
 	public static String readStringData(int i ,int j) throws IOException { //create an excel
@@ -20,8 +20,8 @@ public class ExcelCode {
 		
 		wb= new XSSFWorkbook(fs); //enni aa fs iney xlsxf workbook ilote store aakum
 		//interface aanu enni
-		sh= wb.getSheet("Sheet1"); //sheet name 
-		Row r= sh.getRow(i); //row number
+		s= wb.getSheet("Sheet1"); //sheet name 
+		Row r= s.getRow(i); //row number
 		Cell c= r.getCell(j); //cell number
 		return c.getStringCellValue();// ee method name inbuild ayi varunathe aanu
 	}
@@ -29,8 +29,8 @@ public class ExcelCode {
 		//value koduthalum java il  double ayitey eduku
 		fs= new FileInputStream("D:\\mavenexcels\\Book2.xlsx"); 
 		wb= new XSSFWorkbook(fs); 
-		sh= wb.getSheet("Sheet1"); 
-		Row r= sh.getRow(i); 
+		s= wb.getSheet("Sheet1"); 
+		Row r= s.getRow(i); 
 		Cell c= r.getCell(j); 
 		return c.getNumericCellValue();
 		
